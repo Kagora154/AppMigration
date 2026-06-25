e96e06a975a643a6ef7f687b0f76235dd99cf4b0
kagora <aosa7778@gmail.com>
2026-06-25 13:01:07 +0300

    test: trigger CI pipeline

--- app/src/main/java/dev/appmigration/AppMigrationApplication.java
+++ app/src/main/java/dev/appmigration/AppMigrationApplication.java
@@ -1,4 +1,5 @@
 package dev.appmigration;
+//thats all the code in this file, it is just the main class to run the application
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
